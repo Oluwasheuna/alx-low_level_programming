4937f3467f25527a77ec83be1af6fe13c5de99ec
Oluwaseun <olusegun.akinwunmi@gmail.com>
2023-06-16 01:49:27 -0700

    6-size.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This is my first C program
+ * return: 0 (Success)
+ */	
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}				
---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the different type of computer types
+ * Return 0 (Success)
+*/
+
+init main(void)
+{	
+	
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+printf("Size of char: %lu byte(s)\n", (unsigned long)sizeofof(a);
+printf("Size of int: %lu byte(s)\n", (unsigned long)sizeofof(b);
+printf("Size of long init: %lu byte(s)\n", (unsigned long)sizeofof(c);
+printf("Size of long long init: %lu byte(s)\n", (unsigned long)sizeofof(d);
+printf("Size of float: %lu byte(s)\n", (unsigned long)sizeofof(f);
+return (0);
+}
