58e7f60157f30f7a985555266e5ec4ca2906ddf9
Oluwaseun <olusegun.akinwunmi@gmail.com>
2023-06-19 10:37:19 -0700

    5-print_numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Result 0 (Valid)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 0; x < 10; x++)
+		printf("%d", x);
+	putchar('\n');
+	return (0);
+}
