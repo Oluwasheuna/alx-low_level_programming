a25652360a201bc31ad08f6aebcd9f92ed5ed174
Oluwaseun <olusegun.akinwunmi@gmail.com>
2023-06-19 10:55:10 -0700

    7-print_tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: 0
+ */
+
+int main(void)
+{
+	char x;
+
+	x = 'z';
+	while
+		(x >= 'y') {
+			putchar(x);
+			x--;
+		}
+	putchar('\n');
+	return (0);
+}
