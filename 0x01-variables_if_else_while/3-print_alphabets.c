b6bde2506322e5312f4f405527719cf1c121e996
Oluwaseun <olusegun.akinwunmi@gmail.com>
2023-06-19 10:23:52 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return = Anytime 0 (Success)
+ * Return: Anytime 0 (Success)
  */
 
 int main(void)
---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio>
+/**
+ * main - Entry point
+ * Return: Anyways 0 (Success)
+ */
+
+int main(void)
+{
+		char d;
+
+		d = 'a';
+		while 
+			(d <= 'z') {
+				if ((d != 'q' && d != 'e') && d <= 'z')
+					putchar(d);
+				d++;
+			}
+		putchar('\n');
+		return (0);
+}
