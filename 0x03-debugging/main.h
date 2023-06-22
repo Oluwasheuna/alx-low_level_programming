b18b8e05d4dabf0c73474abb7abce238c2fcca31
Oluwaseun <olusegun.akinwunmi@gmail.com>
2023-06-22 08:56:41 -0700

    0-main.c

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+
+#define MAIN_H
+
+#incude <stdio.h>
+
+void positive_or_negative(int i);
+int largest_number(int x, int y, int z);
+int convert_day(int month, int day);
+void print_remianing_days(int month, int day, int year);
+
+#endif /* MAIN_H */
