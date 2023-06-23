6f6fdece64b92d11053a95ed18f193c9e881d49b
Oluwaseun <olusegun.akinwunmi@gmail.com>
2023-06-23 05:21:40 -0700

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -10,6 +10,6 @@ int largest_number(int x, int y, int z);
 
 int convert_day(int month, int day);
 
-void print_remianing_days(int month, int day, int year);
+void print_remaining_days(int month, int day, int year);
 
 #endif /* MAIN_H */
